4e8acb921f829f7850a9b54e79eee58a331044fa
Regulyarniy Nikolay <regularniy@gmail.com>
2018-11-16 07:08:53 +1000

    Реализовал переключение экранов с клавиатуры

--- js/main.js
+++ js/main.js
@@ -12,10 +12,36 @@ const templates = [
 
 const app = document.querySelector(`.main`);
 
+const state = {
+  currentScreen: 0
+};
+
 const showScreen = function (screenIndex) {
   while (app.firstChild) {
-    app.removeChild(app.firstChild)
+    app.removeChild(app.firstChild);
+  }
+  if (screenIndex < 0) {
+    screenIndex = templates.length - 1;
+  }
+  if (screenIndex >= templates.length) {
+    screenIndex = 0;
   }
   const content = document.importNode(templates[screenIndex].content, true);
   app.appendChild(content);
+  state.currentScreen = screenIndex;
 };
+
+document.addEventListener(`DOMContentLoaded`, function () {
+  showScreen(0);
+});
+
+document.addEventListener(`keydown`, function (e) {
+  switch (e.key) {
+    case `ArrowLeft`:
+      showScreen(state.currentScreen - 1);
+      break;
+    case `ArrowRight`:
+      showScreen(state.currentScreen + 1);
+      break;
+  }
+});
