75d69c38c631b674f60e76fbbfb52ca20cb3a0b6
Regulyarniy Nikolay <regularniy@gmail.com>
2018-12-02 16:45:27 +1000

    Описал класс для экрана проигрыша по времени

---
+++ js/views/fail-time-view.js
@@ -0,0 +1,12 @@
+import AbstractFailView from './abstract-fail-view';
+
+export default class FailTimeView extends AbstractFailView {
+  get template() {
+    return `<section class="result">
+  <div class="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"></div>
+  <h2 class="result__title">Увы и ах!</h2>
+  <p class="result__total result__total--fail">Время вышло! Вы не успели отгадать все мелодии</p>
+  <button class="result__replay" type="button">Попробовать ещё раз</button>
+</section>`;
+  }
+}
