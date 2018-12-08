e49af68f09c9bce03c3a0cb330073c2fb858ce80
Regulyarniy Nikolay <regularniy@gmail.com>
2018-12-08 18:39:25 +1000

    Реализовал склонение строки с ошибками на экране результатов

--- js/services/utils.js
+++ js/services/utils.js
@@ -2,15 +2,15 @@ const APP_ROOT_ELEMENT_SELECTOR = `.app`;
 const APP_SCREEN_ELEMENT_SELECTOR = `.main`;
 
 /**
- * Конвертирует число в числительное в множественной форме
+ * Конвертирует число секунд или минут в числительное в множественной форме
  * @example
  * // return `22 минуты`
- * convertNumericToPluralString(22,`минут`);
+ * convertTimeCounterToPluralString(22,`минут`);
  * @param {Number} number Числительное
  * @param {String} numericString
  * @return {*}
  */
-export const convertNumericToPluralString = (number, numericString) => {
+export const convertTimeCounterToPluralString = (number, numericString) => {
   const numberLong = number % 100;
   if (numberLong > 10 && numberLong < 15) {
     return numericString;
@@ -22,6 +22,22 @@ export const convertNumericToPluralString = (number, numericString) => {
       return `${numericString}ы`;
   }
   return numericString;
+};
+/**
+ * Конвертирует число ошибок в строку во множ. форме
+ * @param {Number} mistakes Количество ошибок
+ * @return {string} Возвращает строку вида `4 ошибки`
+ */
+export const convertMistakesToPluralString = (mistakes) => {
+  switch (mistakes) {
+    case 0:
+      return `${mistakes} ошибок`;
+    case 1:
+      return `${mistakes} ошибку`;
+    default:
+      return `${mistakes} ошибки`;
+  }
+
 };
 
 /**
@@ -64,9 +80,9 @@ export default {
     const minutes = Math.floor(seconds / 60);
     const secondsAfterMinutes = seconds % 60;
     let minutesString = ``;
-    const secondsString = `${secondsAfterMinutes} ${convertNumericToPluralString(secondsAfterMinutes, `секунд`)}`;
+    const secondsString = `${secondsAfterMinutes} ${convertTimeCounterToPluralString(secondsAfterMinutes, `секунд`)}`;
     if (minutes > 0) {
-      minutesString = `${minutes} ${convertNumericToPluralString(minutes, `минут`)} и `;
+      minutesString = `${minutes} ${convertTimeCounterToPluralString(minutes, `минут`)} и `;
     }
     return `${minutesString}${secondsString}`;
   }
