f93b69665b144c59ba11a900051f3dcef552ae1b
Regulyarniy Nikolay <regularniy@gmail.com>
2018-12-16 22:25:35 +1000

    eslint fix

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ const mocha = require(`gulp-mocha`);
 const commonjs = require(`rollup-plugin-commonjs`);
 const resolve = require(`rollup-plugin-node-resolve`);
 const babel = require(`rollup-plugin-babel`);
-const uglify = require('gulp-uglify');
+const uglify = require(`gulp-uglify`);
 
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
